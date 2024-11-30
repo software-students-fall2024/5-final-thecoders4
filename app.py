e2127d46574bdbe7d02fd034328c92f391afa188
HannaHan2 <hannahan.021115@gmail.com>
2024-11-30 13:19:27 -0500

    virtual environment setup

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 from flask import Flask, render_template, url_for
 from pymongo import MongoClient
+import os
+
+def create_app():
+    """
+    Create and configure the Flask application
+    Returns: app: the Flask application object
+    """
+    app = Flask(__name__)
+    # mongo setup
+    mongo_uri = os.getenv("MONGO_URI")
+    client = MongoClient(mongo_uri)
+    db = client.database
+    collections = db.collections
+    
