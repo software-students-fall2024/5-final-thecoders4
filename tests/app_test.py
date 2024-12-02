909c97e257ec6ba9202b5dcfbaefcde67939355c
xc528 <xc2344@nyu.edu>
2024-12-02 18:16:12 -0500

    tests for form 3&4

--- tests/app_test.py
+++ tests/app_test.py
@@ -44,3 +44,21 @@ def test_question2(client):  # pylint: disable=redefined-outer-name
     questions= [b"Shedding Level:", b"Coat Grooming Frequency:", b"Drooling Level:"]
     for question in questions:
         assert question in response.data
+
+def test_question3(client):  # pylint: disable=redefined-outer-name
+    response = client.get('/question3')
+    assert response.status_code == 200
+    assert b"Find Your Perfect Dog Companion" in response.data
+    assert b"Choose Your Preference On Following Questions" in response.data
+    questions= [b"Openness to Strangers:", b"Playfulness Level:", b"Watchdog/Protective Nature:", b"Adaptability Level:"]
+    for question in questions:
+        assert question in response.data
+
+def test_question4(client):  # pylint: disable=redefined-outer-name
+    response = client.get('/question4')
+    assert response.status_code == 200
+    assert b"Find Your Perfect Dog Companion" in response.data
+    assert b"Choose Your Preference On Following Questions" in response.data
+    questions= [b"Trainability Level:", b"Energy Level:", b"Barking Level:", b"Mental Stimulation Needs:"]
+    for question in questions:
+        assert question in response.data
